30baf07748186ba4bbeb4ffe3578c4af27ba2bcc
bengkelgadget-code <bengkelgadget.l2@gmail.com>
2026-05-08 09:41:31 +0800

    Update script-pos.js

--- script-pos.js
+++ script-pos.js
@@ -1306,10 +1306,10 @@ function actionSendWA(idOverride) {
                 var estStr = (!allSameDate && itemEst) ? '\n  Selesai: _' + itemEst + '_' : '';
                 
                 if (currentPmb === 'Potong Kuota' && item.satuan === 'Kg') {
-                    txt += '- ' + item.nama + '*\n';
+                    txt += '- ' + item.nama + '*\n'
                     ' (' + item.qty + ' ' + item.satuan + ')' + estStr + '\n\n';
                 } else {
-                    txt += '- ' + item.nama + '*\n';
+                    txt += '- ' + item.nama + '*\n'
                     ' (' + item.qty + ' ' + item.satuan + ' x ' + Number(item.subtotal/item.qty).toLocaleString('id-ID') + ') = Rp ' + Number(item.subtotal).toLocaleString('id-ID') + estStr + '\n\n';
                 }
             });
